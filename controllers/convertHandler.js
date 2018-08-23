8433cc81dd6c63a52696a76c5508b99e7d3400b0
Glitch (farolanf-boilerplate-project-metricimpconverter) <none>
2018-08-23 09:07:31 +0000

    💦🎄 Checkpoint ./controllers/convertHandler.js:1601241/97 ./tests/2_functional-tests.js:1601241/1384 ./server.js:1601241/20

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -58,6 +58,7 @@ function ConvertHandler() {
   };
   
   this.convert = function(initNum, initUnit) {
+    if (isNaN(initNum)) return initNum
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,13 +63,31 @@ suite('Functional Tests', function() {
       });  
       
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: '3.7.2/4kilomegagram' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 'invalid number')
+            assert.equal(res.body.initUnit, 'invalid unit')
+            assert.equal(res.body.returnNum, 'invalid number')
+            assert.equal(res.body.returnUnit, 'invalid unit')
+            done();
+          })
       });
       
       test('Convert kg (no number)', function(done) {
-        
-        //done();
+        chai.request(server)
+          .get('/api/convert')
+          .query({ input: 'kg' })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 1)
+            assert.equal(res.body.initUnit, 'kg')
+            assert.approximately(res.body.returnNum, 2.20462442, 0.1)
+            assert.equal(res.body.returnUnit, 'lbs')
+            done();
+          })
       });
       
     });
